77a7d3227c47aa1eb6af8b96abe5a616c24a434c
atg25 <agard1205@gmail.com>
2025-03-11 15:29:41 -0400

    design updates 3/11

--- src/App.js
+++ src/App.js
@@ -41,134 +41,115 @@ function App() {
 
   return (
     <div className="App">
-      <div
-        id="header"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <h1>How can we foster diversity in the STEM Field?</h1>
-      </div>
-      <h1>What minority has shown the most growth in STEM Degrees?</h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+      <header className="App-header">
+        <h1>How can we foster diversity in STEM Programs?</h1>
+      </header>
+      <main>
+        <section className="card">
+          <h2>What minority has shown the most growth in STEM Degrees?</h2>
+          <div className="chart-container">
+            <div className="chart">
+              <h3>STEM Degrees Awarded from 2011-2021 by Race</h3>
+              <D3TimeSeriesChart />
+            </div>
+            <div className="chart">
+              <CurrentChart />
+              <div className="slider-container">
+                <input
+                  type="range"
+                  min="0"
+                  max={charts.length - 1}
+                  value={currentChartIndex}
+                  onChange={(e) => setCurrentChartIndex(Number(e.target.value))}
+                  className="slider"
+                />
+              </div>
+            </div>
+            <div className="chart">
+              <h3>Growth of Minority Demographics Over the Decade</h3>
+              <GrowthBarChart data={growthData} />
+            </div>
+          </div>
+        </section>
+
+        <section className="card">
+          <h2>We see that hispanics have had the most growth, why is this?</h2>
+          <h3>1. Is it just that there's more college aged hispanics?</h3>
           <div className="chart">
-            <h2>STEM Degrees Awarded from 2011-2021 by Race</h2>
-            <D3TimeSeriesChart />
+            <h4>College-Aged Hispanic Students from 2011-2021</h4>
+            <HispanicCollegeAgeChart />
+            <p>
+              While there is a slight growth in this metric, it is not nearly as
+              steep as the growth in STEM degrees awarded (9% vs 106%). This
+              suggests that there are other factors at play.
+            </p>
+          </div>
+
+          <h3>2. Has there been a rise in HSIs?</h3>
+          <div className="info">
+            <p>
+              HSI stands for hispanic serving institution. These are colleges or
+              universities whose full-time undergraduate population is at least 25%
+              hispanic
+            </p>
           </div>
           <div className="chart">
-            <CurrentChart />
-            <input
-              type="range"
-              min="0"
-              max={charts.length - 1}
-              value={currentChartIndex}
-              onChange={(e) => setCurrentChartIndex(Number(e.target.value))}
-              style={{
-                width: "400px",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "10px",
-              }} // Adjust margin to move the slider closer
-            />
+            <h4>HSIs from 2011-2021</h4>
+            <HSIChart />
+            <p>
+              There had been a significant growth in HSIs (approx. 60%). This means
+              there are more colleges that are more suitable and accessible to
+              hispanic students.
+            </p>
           </div>
-        </div>
-        <div className="chart">
-          <h2>Growth of Minority Demographics Over the Decade</h2>
-          <GrowthBarChart data={growthData} />
-        </div>
-      </div>
-      <h1>We see that hispanics have had the most growth, why is this?</h1>
-      <h2> 1. Is it just that there's more college aged hispanics?</h2>
-      <div className="chart" style={{ width: "800px" }}>
-        <h2>College-Aged Hispanic Students from 2011-2021</h2>
-        <HispanicCollegeAgeChart />
-        <p>
-          While there is a slight growth in this metric, it is not nearly as
-          steep as the growth in STEM degrees awarded (9% vs 106%). This
-          suggests that there are other factors at play.
-        </p>
-      </div>
+          <div className="info">
+            <p>
+              Though there is no specific data on HSIs and their STEM Programs, the
+              NSF has dedicated increasing funding towards HSIs over the period.
+              With this increased funding, STEM programs and research at HSIs have
+              been improved, which can lead to more hispanic students pursuing STEM
+              degrees.
+            </p>
+          </div>
 
-      <h2> 2. Has there been a rise in HSIs?</h2>
-      <div className="info">
-        <p>
-          HSI stands for hispanic serving institution. These are colleges or
-          universities whose full-time undergraduate population is at least 25%
-          hispanic
-        </p>
-      </div>
-      <div className="chart" style={{ width: "800px" }}>
-        <h2>HSIs from 2011-2021</h2>
-        <HSIChart />
-        <p>
-          There had been a significant growth in HSIs (approx. 60%). This means
-          there are more colleges that are more suitable and accessible to
-          hispanic students.
-        </p>
-      </div>
-      <div className="info">
-        <p>
-          Though there is no specific data on HSIs and their STEM Programs, the
-          NSF has dedicated increasing funding towards HSIs over the period.
-          With this increased funding, STEM programs and research at HSIs have
-          been improved, which can lead to more hispanic students pursuing STEM
-          degrees.
-        </p>
-      </div>
+          <h3>3. Are there more scholarships/grants for hispanic students in STEM programs?</h3>
+          <div className="info">
+            <p>
+              Scholarships are a great way to help students pay for college. The
+              more scholarships available to hispanics, the more likely they are to
+              pursue a college degree. Grants help schools pay for new programs and
+              equipment to enrich students' experience without increasing tuition.
+            </p>
+          </div>
 
-      <h2>
-        {" "}
-        3. Are there more scholarships/grants encouraging hispanic students to
-        pursue STEM?
-      </h2>
-      <div className="info">
-        <p>
-          Scholarships and grants are a great way to help students pay for
-          college. The more scholarships/grants available to hispanics, the more
-          likely they are to pursue a college degree.
-        </p>
-      </div>
+          <h3>4. Are STEM programs more accessible to hispanic students?</h3>
+          <div className="info">
+            <p>
+              Regional initiatives are programs that are designed to help students
+              in a specific region. These programs can help students get the
+              resources they need to succeed in college. (Google Maps here)
+            </p>
+          </div>
+        </section>
 
-      <h2>4. Are STEM programs more accessible to hispanic students?</h2>
-      <div className="info">
-        <p>
-          Regional initiatives are programs that are designed to help students
-          in a specific region. These programs can help students get the
-          resources they need to succeed in college. (Google Maps here)
-        </p>
-      </div>
-
-      <h1>How can we help other minorities?</h1>
-      <div className="info">
-        <p>
-          Obviously the efforts to help hispanics have been successful, but what
-          about other minorities? We can take the lessons learned from the
-          hispanic initiatives and apply them to other minority groups.
-        </p>
-        <ul>
-          <li>Have a sort of HSI classification for other demographics</li>
-          <li>
-            Look at regional initiatives and apply the most successful ones
-            nationally
-          </li>
-        </ul>
-      </div>
+        <section className="card">
+          <h2>How can we help other minorities?</h2>
+          <div className="info">
+            <p>
+              Obviously the efforts to help hispanics have been successful, but what
+              about other minorities? We can take the lessons learned from the
+              hispanic initiatives and apply them to other minority groups.
+            </p>
+            <ul>
+              <li>Have a sort of HSI classification for other demographics</li>
+              <li>
+                Look at regional initiatives and apply the most successful ones
+                nationally
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
